f59a3592f64018724571c41421ee93f9638d892a
Alfonso Mulsa <amulsa@gmail.com>
2024-07-30 21:57:31 +0200

    Loading spinner y debounce en by-pais y en by-region

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
-
 @Component({
   selector: 'countries-by-capital-page',
   templateUrl: './by-capital-page.component.html',
@@ -17,6 +16,7 @@ export class ByCapitalPageComponent {
   constructor( private countriesService: CountriesService ) {}
 
   searchByCapital( term: string): void {
+
     this.isLoading = true;
 
     this.countriesService.searchCapital( term )
--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
@@ -10,13 +11,18 @@ import { CountriesService } from '../../services/countries.service';
 export class ByCountryPageComponent {
 
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
-  constructor( private countriesService: CountriesService) {}
+  constructor( private countriesService: CountriesService ) {}
 
   searchByCountry( term: string): void {
+
+    this.isLoading = true;
+
     this.countriesService.searchCountry( term )
       .subscribe( countries => {
         this.countries = countries;
+        this.isLoading = false;
       });
   }
 

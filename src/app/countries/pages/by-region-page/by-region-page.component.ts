8cda0803f6e8b64797ddbe0cac81af048bf46645
Alfonso Mulsa <amulsa@gmail.com>
2024-07-31 10:55:11 +0200

    Modificación en by-region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,10 +13,14 @@ export class ByRegionPageComponent {
 
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public regions: Region[] = [ 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania' ];
+  public selectedRegion?: Region;
 
   constructor( private countriesService: CountriesService) {}
 
-  searchByRegion( region: string): void {
+  searchByRegion( region: Region): void {
+
+    this.selectedRegion = region;
 
     this.isLoading = true;
 
